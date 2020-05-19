3b3c1b18bb6e299fa5ba5d565f5099612f884205
unknown <valeria.avila17@gmail.com>
2020-05-19 12:57:54 -0700

    Added isTextPresent() to validate data

--- ts/main.ts
+++ ts/main.ts
@@ -5,5 +5,23 @@ window.onload = function(){
 }
 
 function main():void{
-    alert("Reg button was clicked");
+    isTextPresent("first-name","First name is required");
+    isTextPresent("last-name","Last name required");
 }
+
+/**
+ * Return true if the text box with the given id has some text inside of it
+ * @param id the id of the <input type="text"> to validate
+ * @param errMsg the message to display in the sibling span of the textbox
+ */
+function isTextPresent(id:string, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+    let txtBoxValue = txtBox.value;
+    if (txtBoxValue == "") {
+        let errSpan = <HTMLElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
+}
+
